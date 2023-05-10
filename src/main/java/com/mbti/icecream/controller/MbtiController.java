126648af35a66d715b27da37579e210dfe40c8f7
khbbbbi <gkaqk0626@naver.com>
2023-05-10 16:21:54 +0900

    #6 MBTI 결과 저장-코드 수정

--- src/main/java/com/mbti/icecream/controller/MbtiController.java
+++ src/main/java/com/mbti/icecream/controller/MbtiController.java
@@ -18,14 +18,14 @@ public class MbtiController {
 
     @PostMapping("/result")
     public String calmbti(MbtiDto mbtiDto){
-        //dto로 받아온 데이터로 service한테 mbti를 계산하게 하고, 결과 값을 받아옴.
+        // dto로 받아온 데이터로 service한테 mbti를 계산하게 하고, 결과 값을 받아옴.
         String result = mbtiService.calResult(mbtiDto);
 
         // Dto를 Entity화
         MbtiResult mbtiResult = mbtiDto.toEntity();
 
-        // Repository한테 Entity를 DB안에 저장하게 함.
-        MbtiResult saved = mbtiRepository.save(mbtiResult);
+        // Service한테 넘겨서 Repository한테 Entity를 DB안에 저장하게 함.
+        mbtiService.save(mbtiResult);
 
         return result;
     }
